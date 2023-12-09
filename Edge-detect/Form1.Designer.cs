9a4f59bc7edf9c13f876cf455feddad2c12d3314
lionprogram <mahdi6269@gmail.com>
2023-12-09 10:14:38 -0800

    Add sobel and robert Edge detect

--- Edge-detect/Form1.Designer.cs
+++ Edge-detect/Form1.Designer.cs
@@ -39,6 +39,8 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -95,7 +97,7 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(643, 82);
+            this.radioButton1.Location = new System.Drawing.Point(643, 210);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(61, 17);
             this.radioButton1.TabIndex = 10;
@@ -106,7 +108,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(643, 105);
+            this.radioButton2.Location = new System.Drawing.Point(643, 233);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(70, 17);
             this.radioButton2.TabIndex = 11;
@@ -154,11 +156,33 @@
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(643, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 43);
+            this.button2.TabIndex = 12;
+            this.button2.Text = " Robert لبه یابی ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(643, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 43);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Sobel لبه یابی";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(749, 625);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label4);
@@ -194,6 +218,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Edge-detect/Form1.cs
+++ Edge-detect/Form1.cs
@@ -19,34 +19,25 @@ namespace Edge_detect
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //clear pictures box
+            pictureBoxCleaner();
             Bitmap originalImage = new Bitmap(pictureBox1.Image);
             Bitmap resultX = new Bitmap(pictureBox1.Image);
             Bitmap resultY = new Bitmap(pictureBox1.Image);
             Bitmap resultXY = new Bitmap(pictureBox1.Image);
             Color currentPix,nextPixelX,nextPixelY,sumX,sumY,sumXY;
             int resX,resY,res;
-            for (int x = 0; x < originalImage.Width-1; x++)
+            for (int x = 0; x <= originalImage.Width-2; x++)
             {
-                for (int y = 0; y < originalImage.Height-1; y++)
+                for (int y = 0; y <= originalImage.Height-2; y++)
                 {
                     currentPix = originalImage.GetPixel(x, y);
                     nextPixelX = originalImage.GetPixel(x+1, y);
                     nextPixelY = originalImage.GetPixel(x , y+1);
-                    resX = currentPix.R - nextPixelX.R;
-                    resY = currentPix.R - nextPixelY.R;
-                    if (radioButton1.Checked) {
-                        res = Convert.ToInt32(Math.Sqrt(Math.Ceiling(Math.Pow(resX, 2) + Math.Pow(resY, 2))));
-                    }
-                    else
-                    {
-                        res = resX + resY;
-                    }
-                    // check resX betwen 0 to 255
-                    resX = CheckRange(resX);
-                    // check resY betwen 0 to 255
-                    resY = CheckRange(resY);
-                    // check res betwen 0 to 255
-                    res = CheckRange(res);
+                    resX = Math.Abs(currentPix.R - nextPixelX.R);
+                    resY = Math.Abs(currentPix.R - nextPixelY.R);
+                    //check radio button and check res between 0 to 255
+                    res = checkhandler(resX, resY);
 
                     sumX = Color.FromArgb(resX, resX, resX);
                     sumY = Color.FromArgb(resY, resY, resY);
@@ -61,22 +52,130 @@ namespace Edge_detect
             pictureBox4.Image = resultXY;
         }
 
-        private int CheckRange(int pixel)
+        private void button2_Click(object sender, EventArgs e)
         {
-            int resualt;
-            if (pixel > 255)
+            //clear pictures box
+            pictureBoxCleaner();
+            Bitmap originalImage = new Bitmap(pictureBox1.Image);
+            Bitmap resultX = new Bitmap(pictureBox1.Image);
+            Bitmap resultY = new Bitmap(pictureBox1.Image);
+            Bitmap resultXY = new Bitmap(pictureBox1.Image);
+            Color currentPix, nextPixelZ, nextPixelX, nextPixelY, sumX, sumY, sumXY;
+            int resX, resY, res;
+            for (int x = 0; x <= originalImage.Width - 2; x++)
             {
-                resualt = 255;
+                for (int y = 0; y <= originalImage.Height - 2; y++)
+                {
+                    //edge detect X
+                    currentPix = originalImage.GetPixel(x, y);
+                    nextPixelZ = originalImage.GetPixel(x+1,y+1);
+                    resX = Math.Abs(currentPix.R - nextPixelZ.R);
+                    //edge detect Y
+                    nextPixelX = originalImage.GetPixel(x+1,y);
+                    nextPixelY = originalImage.GetPixel(x,y+1);
+                    resY = Math.Abs(nextPixelX.R - nextPixelY.R);
+                    //check radio button and check res between 0 to 255
+                    res = checkhandler(resX,resY);
+
+                    sumX = Color.FromArgb(resX, resX, resX);
+                    sumY = Color.FromArgb(resY, resY, resY);
+                    sumXY = Color.FromArgb(res, res, res);
+                    resultX.SetPixel(x, y, sumX);
+                    resultY.SetPixel(x, y, sumY);
+                    resultXY.SetPixel(x, y, sumXY);
+                }
             }
-            else if (pixel < 0)
-            {
-                resualt = 0;
-            }
-            else {
-                return pixel;
-            }
-            return resualt;
-            
+            pictureBox2.Image = resultX;
+            pictureBox3.Image = resultY;
+            pictureBox4.Image = resultXY;
         }
+
+        private void pictureBoxCleaner()
+        {
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+            pictureBox4.Image = null;
+        }
+
+        private int checkhandler(int resX, int resY)
+        {
+            int res;
+            //radiobutoon check
+            if (radioButton1.Checked)
+            {
+                res = Convert.ToInt32(Math.Sqrt(Math.Ceiling(Math.Pow(resX, 2) + Math.Pow(resY, 2))));
+            }
+            else
+            {
+                res = resX + resY;
+            }
+            // check res betwen 0 to 255
+            res = checkTrueVal(res);
+            
+            return res;
+        }
+
+        private int checkTrueVal(int res)
+        {
+            if (res > 255)
+            {
+                res = 255;
+            }
+            return res;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //clear pictures box
+            pictureBoxCleaner();
+            Bitmap originalImage = new Bitmap(pictureBox1.Image);
+            Bitmap resultX = new Bitmap(pictureBox1.Image);
+            Bitmap resultY = new Bitmap(pictureBox1.Image);
+            Bitmap resultXY = new Bitmap(pictureBox1.Image);
+            Color currentPix,previousPixelX1, previousPixelX2, previousPixelX3, nextPixelX1, nextPixelX2, nextPixelX3, sumX, sumY, sumXY;
+            Color previousPixelY1, previousPixelY2, previousPixelY3, nextPixelY1, nextPixelY2, nextPixelY3;
+            int resX, resY, res,temp;
+            for (int x = 1; x <= originalImage.Width - 2; x++)
+            {
+                for (int y = 1; y <= originalImage.Height - 2; y++)
+                {
+                    //edge detect X
+                    currentPix = originalImage.GetPixel(x, y);
+
+                    previousPixelX1 = originalImage.GetPixel(x - 1, y - 1);
+                    previousPixelX2 = originalImage.GetPixel(x - 1, y );
+                    previousPixelX3 = originalImage.GetPixel(x - 1, y + 1);
+                    nextPixelX1 = originalImage.GetPixel(x + 1,y - 1);
+                    nextPixelX2 = originalImage.GetPixel(x + 1, y);
+                    nextPixelX3 = originalImage.GetPixel(x + 1, y + 1);
+                    temp = -previousPixelX1.R - previousPixelX2.R - previousPixelX3.R + nextPixelX1.R + nextPixelX2.R + nextPixelX3.R;
+                    resX = Math.Abs(temp);
+                    resX = checkTrueVal(resX);
+                    //edge detect Y
+                    previousPixelY1 = originalImage.GetPixel(x - 1, y - 1);
+                    previousPixelY2 = originalImage.GetPixel(x , y - 1);
+                    previousPixelY3 = originalImage.GetPixel(x + 1, y - 1);
+                    nextPixelY1 = originalImage.GetPixel(x - 1, y + 1);
+                    nextPixelY2 = originalImage.GetPixel(x , y + 1);
+                    nextPixelY3 = originalImage.GetPixel(x + 1, y + 1);
+                    temp = -previousPixelY1.R - previousPixelY2.R - previousPixelY3.R + nextPixelY1.R + nextPixelY2.R + nextPixelY3.R;
+                    resY = Math.Abs(temp);
+                    resY = checkTrueVal(resY);
+                    //check radio button and check res between 0 to 255
+                    res = checkhandler(resX, resY);
+
+                    sumX = Color.FromArgb(resX, resX, resX);
+                    sumY = Color.FromArgb(resY, resY, resY);
+                    sumXY = Color.FromArgb(res, res, res);
+                    resultX.SetPixel(x, y, sumX);
+                    resultY.SetPixel(x, y, sumY);
+                    resultXY.SetPixel(x, y, sumXY);
+                }
+            }
+            pictureBox2.Image = resultX;
+            pictureBox3.Image = resultY;
+            pictureBox4.Image = resultXY;
+        }
+    
     }
 }
